fb463e11188d8dab161b0a12445fc3cf54e2b0d5
Liam <liam@tengelisconsulting.com>
2025-04-21 15:02:35 -0700

    v0.1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="expert_llm",
-    version="v0.1.8",
+    version="v0.1.9",
     description="LLM client for 'expert'",
     author="Liam Tengelis",
     author_email="liam.tengelis@blacktuskdata.com",
